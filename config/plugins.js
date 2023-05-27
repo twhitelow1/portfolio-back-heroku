8ecc7a0579ce742c09aa73be67a1a25c1688d64d
Todd Whitelow <todd@whitelow.tech>
2023-05-27 01:45:02 -0600

    add the aws credentials

--- config/plugins.js
+++ config/plugins.js
@@ -18,7 +18,24 @@ module.exports = ({ env }) => ({
         uploadStream: {
           ACL: null
         },
-      }
-    },
-  }
-});
+        upload: {
+          config: {
+            provider: 'aws-s3',
+            providerOptions: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+              region: env('AWS_REGION'),
+              params: {
+                ACL: 'private', // <== set ACL to private
+                signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 60 * 60 * 24 * 7),
+                Bucket: env('AWS_BUCKET'),
+              },
+            },
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+            },
+          },
+        }
+      });
