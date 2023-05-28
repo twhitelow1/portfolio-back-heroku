fe95351811200e514a1976b394e9f989f4dbaa24
Todd Whitelow <todd@whitelow.tech>
2023-05-28 01:25:30 -0600

    set cloudinary as provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,41 +1,18 @@
 module.exports = ({ env }) => ({
+  // ...
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: 'cloudinary',
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET_NAME'),
-        },
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
-      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
       actionOptions: {
-        upload: {
-          ACL: null
-        },
-        uploadStream: {
-          ACL: null
-        },
-        upload: {
-          config: {
-            provider: 'aws-s3',
-            providerOptions: {
-              accessKeyId: env('AWS_ACCESS_KEY_ID'),
-              secretAccessKey: env('AWS_ACCESS_SECRET'),
-              region: env('AWS_REGION'),
-              params: {
-                ACL: 'private', // <== set ACL to private
-                signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 60 * 60 * 24 * 7),
-                Bucket: env('AWS_BUCKET'),
-              },
-            },
-            actionOptions: {
-              upload: {},
-              uploadStream: {},
-              delete: {},
-            },
-          },
-        }
-      });
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
